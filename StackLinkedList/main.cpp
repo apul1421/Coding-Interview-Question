98c0b549354fab68e5c8303b5e2c42a2722758a6
apul <apul.gupta1421@gmail.com>
2017-08-15 17:36:27 +0530

    Initial Commit

---
+++ StackLinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StackLinkedList
+//
+//  Created by Apul Gupta on 15/08/17.
+//  Copyright © 2017 Apul Gupta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
