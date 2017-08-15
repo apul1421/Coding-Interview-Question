e213cc5379b1eb58a97217c1f5d3a56fdf296ca8
apul <apul.gupta1421@gmail.com>
2017-08-15 20:23:31 +0530

    PUSH and POP in a stack using List

--- StackLinkedList/main.cpp
+++ StackLinkedList/main.cpp
@@ -34,14 +34,77 @@ public:
         top = NULL;
         count=0;
     }
+
+    void Push(int element){
+        
+        Node* temp = new Node();
+        temp->data=element;
+        temp->link=top;
+        top=temp;
+        count++;
+        
+    }
+    
+    int Pop(){
+        if(top==NULL){
+            throw new StackUnderFlowException;
+        }
+        int ret = top->data;
+        Node* temp = top->link;
+        delete top;
+        top = temp;
+        count--;
+        return ret;
+        
+    }
+    
+    int Top(){
+        
+        return top->data;
+    }
     
     
+    int Size(){
+        
+        return count;
+    }
     
+    bool isEmpty(){
+        
+        return (top=NULL) ? true:false;
+    }
     
 };
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ListStack s;
+    try {
+        if(s.isEmpty()){
+            cout<<"Stack is Empty";
+        }
+    
+        //Push Elements
+        s.Push(100);
+        s.Push(200);
+        
+        //Size Of Stack
+        cout<<"Size of the stack is:"<<s.Size()<<endl;
+        
+        //Top Element
+        cout<<"Top Element of Stack:"<<s.Top()<<endl;
+        
+        //Pull Element
+        cout<<"Poping of element starts"<<endl;
+        cout<<s.Pop()<<endl;
+        cout<<s.Pop()<<endl;
+        cout<<s.Pop()<<endl;
+        
+    }
+    
+    catch (...) {
+        
+        cout<<"Some unexpected error occured"<<endl;
+    }
+
     return 0;
 }
